d544dd68792f693449daa3f05d87892c142379d6
Ville Sirviö <VSirvio@users.noreply.github.com>
2024-09-10 08:58:01 +0300

    Print 'Connected to MongoDB' message to log instead of using fmt.Println() in backend/main.go

--- backend/main.go
+++ backend/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 
@@ -50,7 +49,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println("Connected to MongoDB")
+	log.Println("Connected to MongoDB")
 
 	collection = client.Database("library").Collection("books")
 
